a8a468ee58c751ce5dc8db18b767fa6b29f6adac
cshipley98 <claireshipley98@gmail.com>
2024-02-08 17:31:05 -0600

    js variables to reference DOM

---
+++ index.js
@@ -0,0 +1,11 @@
+// API KEY 
+apiKey = "581ce92b85fceec3c210d9faa500eccb"
+
+// variables to reference DOM
+dailyWeatherEL = document.querySelector("#daily-weather")
+searchBtn = document.querySelector("#search-btn");
+cityNameInput = document.querySelector("#cityName");
+forcastWeather = document.querySelector("#forecast-weather");
+citySearchName = document.querySelector("#city-search-term");
+cityList = document.querySelector("#city-list");
+clearHistoryBtn = document.querySelector("#clear-history-btn");
